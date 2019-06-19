12266b65aaaba23e37b9867e2b76b2ead9a961d7
xfrag <mourad.alibi@enis.tn>
2019-06-19 11:26:13 +0100

    creat back-end project

---
+++ views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    if request.method == 'GET':
+        return HttpResponse("Hello, world. You're at the polls index.")
+    elif request.method == 'POST':
+        return HttpResponse("Hello, world. You're at the polls index.")
